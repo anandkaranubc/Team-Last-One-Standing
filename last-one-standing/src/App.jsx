34708083ab1055953c7a328576eecd2de36486d5
Manan Daga <manandaga2004@gmail.com>
2024-10-05 21:25:13 -0700

    Add Dashboard

--- last-one-standing/src/App.jsx
+++ last-one-standing/src/App.jsx
@@ -2,8 +2,9 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import AuthPage from "./components/AuthPage"; // Make sure the path is correct
-import reactLogo from "./assets/react.svg"; 
+import reactLogo from "./assets/react.svg";
 import AboutUs from "./components/about";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/auth" element={<AuthPage />} />
         <Route path="/" element={<AboutUs />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         {/* You can add more routes here */}
       </Routes>
     </Router>
