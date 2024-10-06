c67c3dc650398b28d0a37307a673f90b560c4349
amahaj2 <daveblaine550@gmail.com>
2024-10-05 21:11:45 -0700

    finalised app.jsx and about.jsx and auth.jsx

--- last-one-standing/src/App.jsx
+++ last-one-standing/src/App.jsx
@@ -2,37 +2,15 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import AuthPage from "./components/AuthPage"; // Make sure the path is correct
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import reactLogo from "./assets/react.svg"; 
 import AboutUs from "./components/about";
-
-function Home() {
-  return (
-    <>
-      <div>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        
-        <Link to="/about">
-          <button>Welcome to the Home Page!</button>
-        </Link>
-      </div>
-      <p className="read-the-docs">Click on the React logo to learn more</p>
-    </>
-  );
-}
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="/auth" element={<AuthPage />} />
-        <Route path="/about" element={<AboutUs />} />
+        <Route path="/" element={<AboutUs />} />
         {/* You can add more routes here */}
       </Routes>
     </Router>
